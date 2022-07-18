e098ee5a8e501044cc9959f77f3a177c2a3c4354
Diego Veiga <diegogv.eng@gmail.com>
2022-07-18 20:17:48 -0300

    Token expiration tests

--- base/tests.py
+++ base/tests.py
@@ -1,25 +1,46 @@
+from freezegun import freeze_time
+from datetime import datetime
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
 # Create your tests here.
 
+class FakeDateTime(datetime):
+    "A manipulable datetime replacement"
+    def __new__(cls, *args, **kwargs):
+        return datetime.__new__(datetime, *args, **kwargs)
+
+
 class MyTests(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.client = Client()
+        self.responses = {
+            '401 Unauthorized - Token not valid': {
+                "detail": "Given token not valid for any token type",
+                "code": "token_not_valid",
+                "messages": [
+                    {
+                        "token_class": "AccessToken",
+                        "token_type": "access",
+                        "message": "Token is invalid or expired"
+                    }
+                ]
+            }
+        }
 
     def _create_simple_user(self):
         user = User.objects.create_user(username='user_001', email='user_001@example.com', password='user_001_password', is_active=True)
         user.save()
 
-    def test__unauthorized_acess__expected_error_401(self):
+    def test__unauthorized_access__expected_error_401(self):
         c = Client()
         response = c.get('/api/')
         self.assertEqual(401, response.status_code)
         self.assertEqual('Authentication credentials were not provided.', response.json()['detail'])
 
-    def test__authorized_acess_with_token__expected_success(self):
+    def test__authorized_access_with_token__expected_success(self):
         self._create_simple_user()
 
         user = User.objects.get(username='user_001')
@@ -43,3 +64,59 @@ class MyTests(TestCase):
 
         self.assertEqual(200, response_2.status_code)
         self.assertEqual('success', json_response_2['msg'])
+
+    def test__access_not_authorized_with_expired_token__expected_error_401(self):
+        self._create_simple_user()
+
+        token = None
+
+        with freeze_time("2021-01-01 12:00:00"):
+            response = self.client.post('/api/token/', {
+                "username": "user_001",
+                "password": "user_001_password"
+            })
+
+            json_response = response.json()
+            token = json_response['access']
+
+            self.assertFalse(json_response.get('refresh', False) == False)
+            self.assertFalse(json_response.get('access', False) == False)
+
+        # settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] is set to 5 minutes
+        with freeze_time("2021-01-01 12:06:00"):
+            client = APIClient()
+            client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+            response_2 = client.get('/api/', data={'format': 'json'})
+
+            json_response_2 = response_2.json()
+
+            self.assertEqual(401, response_2.status_code)
+            self.assertEqual(self.responses['401 Unauthorized - Token not valid'], json_response_2)
+
+    def test__access_authorized_with_still_valid_token__expected_success(self):
+        self._create_simple_user()
+
+        token = None
+
+        with freeze_time("2021-01-01 12:00:00"):
+            response = self.client.post('/api/token/', {
+                "username": "user_001",
+                "password": "user_001_password"
+            })
+
+            json_response = response.json()
+            token = json_response['access']
+
+            self.assertFalse(json_response.get('refresh', False) == False)
+            self.assertFalse(json_response.get('access', False) == False)
+
+        # settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] is set to 5 minutes
+        with freeze_time("2021-01-01 12:04:00"):
+            client = APIClient()
+            client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+            response_2 = client.get('/api/', data={'format': 'json'})
+
+            json_response_2 = response_2.json()
+
+            self.assertEqual(200, response_2.status_code)
+            self.assertEqual('success', json_response_2['msg'])
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Create your views here.
 
